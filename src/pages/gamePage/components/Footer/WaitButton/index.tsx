11c40053a87ec08d8bad25e1274942c314cac77d
Тимофей Курохтин <forgwent@gmail.com>
2021-11-03 17:11:23 +0300

    Add SkipTurn button

---
+++ src/pages/gamePage/components/Footer/WaitButton/index.tsx
@@ -0,0 +1,17 @@
+import { FC, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import Button from "../../../../../components/Button";
+import { RootState } from "../../../../../store/store";
+import { nextTurn } from "../../../boardSlice";
+
+const SkipTurn: FC = () => {
+  const activeUnit = useSelector((state: RootState) => state.game.activeUnit);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    activeUnit && dispatch(nextTurn({ activeUnit }));
+  };
+  return <Button onClick={handleClick}>Skip turn</Button>;
+};
+
+export default SkipTurn;
--- src/pages/gamePage/components/Footer/index.tsx
+++ src/pages/gamePage/components/Footer/index.tsx
@@ -2,9 +2,15 @@ import { FC, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/store";
 import BattleLog from "./BattleLog";
+import SkipTurn from "./WaitButton";
 
 const Footer: FC = () => {
-  return <BattleLog />;
+  return (
+    <>
+      <BattleLog />
+      <SkipTurn />
+    </>
+  );
 };
 
 export default Footer;
